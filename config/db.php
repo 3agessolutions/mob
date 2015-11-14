92f6e5bd0f6d9ef820fcb5928d0313cfbcf872f6
three_ages <3agesolutions@gmail.com>
2015-11-14 03:42:01 +0000

    Config db.php changes

--- config/db.php
+++ config/db.php
@@ -2,8 +2,27 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=mob',
+    'username' => 'mob',
+    'password' => 'mob@123',
     'charset' => 'utf8',
 ];
+
+
+/*
+SELECT
+  id, (
+    3959 * acos (
+      cos(radians(13.0595) )
+      * cos(radians(vendor_latitude))
+      * cos(radians(vendor_longitude) - radians(80.2425))
+      + sin(radians(13.0595))
+      * sin(radians(vendor_latitude ))
+    )
+  ) AS distance
+FROM mob_location
+HAVING distance < 30
+ORDER BY distance
+LIMIT 0 , 20
+
+*/
